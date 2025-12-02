6a691188bea4606f9bdcac253335bdb5662f0a9d
mausamGurung67 <mausamgurung678@gmail.com>
2025-12-02 11:09:48 +0545

    completed Task 1 successfully

---
+++ Task1/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Task1
+{
+    public class Rectangle
+    {
+        private double length;
+        private double breadth;
+
+        public double Length
+        {
+            get => length;
+            set => length = value;
+        }
+
+        public double Breadth
+        {
+            get => breadth;
+            set => breadth = value;
+        }
+
+        public static double GetArea(double length, double breadth) => length * breadth;
+        // {}
+        public string ShowDetails() => $"Length: {length}, Breadth: {breadth}";
+
+    }
+}
