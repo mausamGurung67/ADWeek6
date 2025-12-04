4961528a7371e456cb6d790334efca689a0de84e
mausamGurung67 <mausamgurung678@gmail.com>
2025-12-04 10:19:05 +0545

    Completed Task 4 successfully

---
+++ Task4/Book.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task4
+{
+    public class Book(string name, double price)
+    {
+        public string Name { get; set; } = name;
+        public double Price { get; set; } = price;
+    }
+}
---
+++ Task4/Student.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task4;
+
+public class Student(string name, DateOnly dob, string gender)
+{
+    public string Name { get; set; } = name;
+    public DateOnly Dob { get; set; } = dob;
+    public string Gender { get; set; } = gender;
+}
